7c2f44dd39ececcf09301a8eca3ed799a1c32ad5
Shah Rasel <shahrasel@gmail.com>
2021-11-16 02:27:35 +0100

    homepage development is done

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\ResProperties;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $cityLists = ResProperties::select(DB::raw('CITY, count(id) as total'))
+            ->where('LISTSTATUS','Active')
+            ->orderBy('total', 'desc')
+            ->groupBy('CITY')
+            ->get();
+        //dd($cityLists);
+        view()->share('cityLists', $cityLists);
     }
 }
